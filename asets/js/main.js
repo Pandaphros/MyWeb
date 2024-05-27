f6b65042b747cef109cef86b03bea1d98f34c8e5
Leandro Monroy <leandro.monroy.t@gmail.com>
2024-05-26 20:06:13 -0400

    Create MediasQueries

--- asets/js/main.js
+++ asets/js/main.js
@@ -2,3 +2,16 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 100);
 });
+
+let menu = document.querySelector("#menu-toggler");
+let navitem = document.querySelector(".nav-item");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navitem.classList.toggle("open");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navitem.classList.remove("open");
+}
